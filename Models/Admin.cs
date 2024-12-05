516fad6bfaf0ea3b0126c062ecd1309f9e07d742
Atheredon <154770934+Atheredon@users.noreply.github.com>
2024-12-05 23:41:38 +0300

    Add files via upload

---
+++ Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KuaförRandevuSistemi.Models
+{
+    public class Admin
+    {
+        public int AdminId { get; set; }
+        [Required]
+        public int UserId { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KuaförRandevuSistemi.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        [Required]
+        public int UserId { get; set; }  // Relationship with User
+
+        [Required]
+        [MaxLength(100)]
+        public string Specialty { get; set; }
+
+        [Required]
+        public int BranchId { get; set; }  // Relationship with Branch
+
+        [Required]
+        public string WorkingHours { get; set; }  // JSON
+
+        public User User { get; set; }
+        public Branch Branch { get; set; }
+    }
+}
