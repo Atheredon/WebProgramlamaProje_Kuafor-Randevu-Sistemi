3f7b1075bb33a7285a51fb50efff6442f1872af9
Atheredon <iukeskin08@gmail.com>
2024-12-10 23:31:46 +0300

    Modeller tekrardan eklendi ve main page yapılmaya başlanıldı.

---
+++ Models/Branch.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KuaförRandevuSistemi.Models
+{
+    public class Branch
+    {
+        public int BranchId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Address { get; set; }
+
+        [Required]
+        public string WorkingHours { get; set; }  // JSON
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KuaförRandevuSistemi.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long.")]
+        public string Password { get; set; }
+
+    }
+}
