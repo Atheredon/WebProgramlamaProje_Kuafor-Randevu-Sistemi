97ea2d40705706179f146976d0e06d2e782892b8
Atheredon <iukeskin08@gmail.com>
2024-12-21 23:46:06 +0300

    Randevu oluşturma yapılıyor. Eleman eklenirken eposta kontrolü eklendi.

--- obj/Debug/net8.0/KuaförRandevuSistemi.AssemblyInfo.cs
+++ obj/Debug/net8.0/KuaförRandevuSistemi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KuaförRandevuSistemi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eee64f723313b0805f46ec5da8b0581cc9008bce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2e59eeb251ca2cb96449a3665a8d7e71248d240")]
 [assembly: System.Reflection.AssemblyProductAttribute("KuaförRandevuSistemi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KuaförRandevuSistemi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
